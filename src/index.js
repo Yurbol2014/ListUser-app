af7185136c6b0b551bb53b8f454d8523cab641d8
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2023-01-08 22:03:33 +0300

    загрузка проекта

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(
-   document.getElementById('root') 
- );
- root.render(
+
+
+   const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
  
  <App />
 
